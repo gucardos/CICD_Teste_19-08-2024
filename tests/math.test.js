ab240e978f1b8ccbea55a5df0dfcfd4e05f97e83
gucardos <134300180+gucardos@users.noreply.github.com>
2024-08-19 23:35:24 +0000

    mais testes

--- tests/math.test.js
+++ tests/math.test.js
@@ -3,3 +3,11 @@ const math = require('../math');
     test('soma 2 + 3 igual a 5', () => { 
         expect(math.add(2, 3)).toBe(5); 
     }); 
+
+    test('soma 3 + 7 igual a 10', () => { 
+        expect(math.add(3, 7)).toBe(10); 
+    }); 
+
+    test('soma 1.1 + 3 igual a 4.1', () => { 
+        expect(math.add(1.1, 3)).toBe(4.1); 
+    }); 
